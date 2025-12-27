1e7e42e1c035cfee16e3c53be1338643201aa151
valeriankaleb <valekaleb46@gmail.com>
2025-12-27 19:53:47 +0700

    second commit

--- app.py
+++ app.py
@@ -46,17 +46,13 @@ app.config['PROFILE_UPLOAD_FOLDER'] = os.path.join(basedir, 'static/img/profiles
 #     password = ""
 # )
 
-db_host = os.environ.get('DB_HOST', 'localhost')
-db_user = os.environ.get('DB_USER', 'root')
-db_pass = os.environ.get('DB_PASSWORD', '')
-db_name = os.environ.get('DB_NAME', 'warung_ronde')
-
 def get_db_connection():
     connection = connector.connect(
-        host=db_host,
-        user=db_user,
-        password=db_pass,
-        database=db_name
+        user="webronde", 
+        password="46Ronde46", 
+        host="webronde.mysql.database.azure.com", 
+        port=3306, 
+        database="warung_ronde"
     )
     return connection
 
